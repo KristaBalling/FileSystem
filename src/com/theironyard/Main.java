68869c68fdf7c2af20d040fbb7ef0340a526db6c
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-14 11:34:16 -0500

    create file in a directory

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -9,18 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            File.delete(fileToDelete);
-//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "Dir1", "file1.txt");
-//            Path destination = FileSystems.getDefault().getPath("Examples", "file1");
-//            Files.move(fileToMove, destination);
-//            Path sourceFile = FileSystems.getDefault().getPath("Examples","Dir1", "file1.txt");
-//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
-//
-//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
-//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+            Path fileToCreate = FileSystems.getDefault().getPath("Examples","file2.txt");
+            Files.createFile(fileToCreate);
 
         } catch (IOException e) {
             System.out.println(e.getMessage());        }
